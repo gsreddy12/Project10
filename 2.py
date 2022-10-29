f778151657ab8d852390f5b6cfcba4689a214962
gsreddy12 <gdr.sateesh12@gmail.com>
2022-10-30 02:52:15 +0530

    initial commit

---
+++ 2.py
@@ -0,0 +1,9 @@
+#Iterative Control Structures
+
+data={1:"sateesh",2:'Hello',3:"Reddy"}
+for item in data:
+    print(item)
+
+gener=['pop','rock','fok']
+for x in range(len(gener)):
+    print("i like ",gener[x])
